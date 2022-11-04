6314dbe3bcc027bbea25741bf3d1cd7d1224e37a
AlexanderDevelopment69 <bryano_98@hotmail.com>
2022-11-04 05:12:11 -0500

    req1,req2,req3 csvFile

--- Tarea1/Libro.py
+++ Tarea1/Libro.py
@@ -87,7 +87,7 @@ class Libro():
 
         # Libro.libros = [*csv.DictReader(open('D:/prueba.csv'))]
         try:
-            with open('D:/prueba.csv', 'r') as data_file:
+            with open('../Tarea1/Libros.csv', 'r') as data_file:
                 data = csv.DictReader(data_file, delimiter=",")
                 for row in data:
                     item = Libro.libros.get(row['id'],
